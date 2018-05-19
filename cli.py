158eec8bfabadd1409f37893fb222271ab99b72f
Payne Gray <rlacsamana@csu.fullerton.edu>
2018-05-19 00:02:57 -0700

    updates

--- cli.py
+++ cli.py
@@ -1,1 +1,42 @@
-import argv
+# *******************************************************************
+# This file illustrates how to send a file using an
+# application-level protocol where the first 10 bytes
+# of the message from client to server contain the file
+# size and the rest contain the file data.
+# *******************************************************************
+import socket
+import os
+import sys
+
+if len(sys.argv)<2:
+	print "Not enough."
+# Server address
+serverAddr = "localhost"
+
+# Server port
+serverPort = int(sys.argv[1])
+
+# Create a TCP socket
+connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+connSock.connect((serverAddr, serverPort))
+
+
+# Keep sending until all is sent
+while True:
+	
+	isay = raw_input("ftp> ")
+
+	if isay=="quit":
+		connSock.send(isay)
+		print "time to go"
+		break
+	else:
+		connSock.send(isay)
+
+# Close the socket and the file
+connSock.close()
+ 	
+
+
--- server.py
+++ server.py
@@ -1,1 +1,52 @@
-import argv
+# *************************************************************
+# This program illustrates how to generate an emphemeral port
+# *************************************************************
+import socket
+import commands
+
+# Create a socket
+welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to port 0
+welcomeSocket.bind(('',0))
+
+# Retreive the ephemeral port number
+print "I chose ephemeral port: ", welcomeSocket.getsockname()[1]
+
+# Start listening on the socket
+welcomeSocket.listen(1)
+
+while True:
+	
+	print "Waiting for connections..."
+	
+	# Accept connections
+	clientSock, addr = welcomeSocket.accept()
+	
+	print "Accepted connection from client: ", addr
+
+	while True:
+		buff = clientSock.recv(100)
+
+		
+
+		print "client> ",buff
+
+
+
+		if buff == "quit":
+			break
+		else:
+			# Run ls command, get output, and print it
+			for line in commands.getstatusoutput(buff):
+				print "client> ",line
+
+
+	break
+welcomeSocket.close()
+
+#get <file name> (downloads file <file name> from the server)
+#put <filename> (uploads file <file name> to the server)
+#ls (lists files on the server)
+#lls (lists files on the client)
+#quit (disconnects from the server and exits)
