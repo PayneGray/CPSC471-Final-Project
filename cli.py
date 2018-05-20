72fbdabc541c5f2b7ea9ec25a4d11fd2893b2d0a
Payne Gray <rlacsamana@csu.fullerton.edu>
2018-05-19 20:21:25 -0700

    breakthrough

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
-import socket
-import os
+from socket import *
 import sys
+
+
 
 '''
 get_file_size (might want a shorter name)
@@ -16,107 +17,86 @@ def get_file_size (file_name):
     file.close()
     return (file_size + len(file_name) + 1)
 
-'''
-putFile Client side version
-Takes: socket to ephemeral port, name of file to send
-returns: number of bytes sent
-'''
-def putFile(socket, file_name):
-    bytes_sent = 0
-    file = open(file_name, "r")
-    # The file data
-    file_data = None
-    # Keep sending until all is sent
-    while True:
-        
-        # Read 65536 bytes of data
-        file_data = file.read(65536)
-        file_data = file_name + bytes(0x1f) + file_data #prepend the file name to the file data with a delimiter of 0x1f which is the ascii code for unit separator 
-        # Make sure we did not hit EOF
-        if file_data:
-            # Get the size of the data read
-            # and convert it to string
-            file_size = len(file_data)
-                        
-            # Send the data!
-            while file_size > bytes_sent:
-                bytes_sent += socket.send(file_data[bytes_sent:])
-        # The file has been read. We are done
-        else:
-            break
-    file.close()
-    return bytes_sent
-
-'''
-takes a socket to receive on, and the number of bytes to receive
-returns the number of bytes received
-the bytes received will be in the form of:
-file_name + 1 byte delimiter + file_data
-This function creates a file using file_name and then writes file_data into it
-'''
-def getFile(sock, numBytes):
-    
-
-    # The buffer
-    recvBuff = ""
-
-    # The temporary buffer
-    tmpBuff = ""
-
-    # Keep receiving till all is received
-    while len(recvBuff) < numBytes:
-        
-        # Attempt to receive bytes
-        tmpBuff =  sock.recv(numBytes)
-        
-        # The other side has closed the socket
-        if not tmpBuff:
-            break
-        
-        # Add the received bytes to the buffer
-        recvBuff += tmpBuff
-    file_size = len(recvBuff)
-    i = recvBuff.find(bytes(0x1f))
-    file_name = recvBuff[0:i]
-    i += 1
-    file_data = recvBuff[i:]
-    file = open(file_name, "w")
-    file.write(file_data)
-    file.close()
-    return file_size
 
 
 
+# ================== CHECK VALID FUNCTION ARGS ================= #
 
-if len(sys.argv)<2:
-	print "Not enough."
-# Server address
-serverAddr = "localhost"
+if len(sys.argv) < 3:
+  print "USAGE python cli.py ", sys.argv[0], " <server machine> <server port>"
+  sys.exit()
 
-# Server port
-serverPort = int(sys.argv[1])
+server_name = sys.argv[1]
+try:
+  server_port = int(sys.argv[2])
+except:
+  print "Error: please enter a valid server port number"
+  sys.exit()
 
-# Create a TCP socket
-connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# ==========  OPEN CONTROL CONNECTION WITH SERVER ============= #
 
-# Connect to the server
-connSock.connect((serverAddr, serverPort))
+try:
+  control_connection = socket(AF_INET, SOCK_STREAM)
+  control_connection.connect((server_name, server_port))
+except Exception as e:
+  print "Could not connect to the specified server"
+  print "Error: ", e
+  sys.exit()
+print 'Opened a control connection with server', (server_name, server_port)
+
+# ===== SEND COMMAND AND OPEN DATA CONNECTION WITH SERVER ==== #
+
+command = ""
+
+while command != "quit":
+
+  #Get command from user
+  command = raw_input('ftp> ')
+
+  #Send length of command (as a 3-character string)
+  command_len_str = str( len(command) ).zfill(3)
+
+  print 'Sending length of the command:', command_len_str
+  bytes_sent = 0
+  while bytes_sent != 3:
+    bytes_sent += control_connection.send( command_len_str[bytes_sent:] )
+  
+  #Length of command has been sent
+  print 'Length of command sent!'
 
 
-
-# Keep sending until all is sent
-while True:
-	command = raw_input("ftp> ")
-
-	if command=="quit":
-		connSock.send(command)
-		print "time to go"
-		break
-	else:
-		connSock.send(command)
-
-# Close the socket and the file
-connSock.close()
- 	
+  #Send command
+  print 'Sending command:', command
+  bytes_sent = 0
+  while bytes_sent != len(command):
+    bytes_sent += control_connection.send( command[bytes_sent:] ) 
+  
+  #Command has been sent
+  print 'Command sent!'
 
 
+  #SEND FILE TO SERVER
+
+  if command.split(" ")[0]=="put":
+    #recieve port num from server
+    port_num = control_connection.recv(5)
+    print "Server sent temp port", port_num+". Connecting now..."
+    temp_socket = socket(AF_INET, SOCK_STREAM)
+    temp_socket.connect((server_name, int(port_num)))
+    print 'Opened a temp connection with server', (server_name, port_num)
+    #DO THE SENDING FILE PART HERE
+    #step 1: make sure the file exists
+
+    temp_socket.close()
+
+  #NOTE: Send command after or before  opening data connection?
+
+  # OPEN DATA CONNECTION #
+
+  # DO FUNCTION #
+
+  # CLOSE DATA CONNECTION #
+
+# CLOSE CONTROL CONNECTION #
+control_connection.close()
+print 'Control connection with', (server_name, server_port), 'closed'
--- server.py
+++ server.py
@@ -1,141 +1,147 @@
-import socket
-import commands
+from socket import *
+import sys
+import os
 
-'''
-get_file_size (might want a shorter name)
-takes a file name
-returns the size of the data to be sent over tcp
-including file size + file name + delimiter character
-'''
-def get_file_size (file_name):
-    file = open(file_name, "r")
-    file_data = file.read(65536)
-    if file_data:
-        file_size = len(file_data)
-    file.close()
-    return (file_size + len(file_name) + 1)
 
-'''
-putFile Client side version
-Takes: socket to ephemeral port, name of file to send
-returns: number of bytes sent
-'''
-def putFile(socket, file_name):
-    bytes_sent = 0
-    file = open(file_name, "r")
-    # The file data
-    file_data = None
-    # Keep sending until all is sent
-    while True:
-        
-        # Read 65536 bytes of data
-        file_data = file.read(65536)
-        file_data = file_name + bytes(0x1f) + file_data #prepend the file name to the file data with a delimiter of 0x1f which is the ascii code for unit separator 
-        # Make sure we did not hit EOF
-        if file_data:
-            # Get the size of the data read
-            # and convert it to string
-            file_size = len(file_data)
-                        
-            # Send the data!
-            while file_size > bytes_sent:
-                bytes_sent += socket.send(file_data[bytes_sent:])
-        # The file has been read. We are done
-        else:
-            break
-    file.close()
-    return bytes_sent
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll(sock, numBytes):
 
-'''
-takes a socket to receive on, and the number of bytes to receive
-returns the number of bytes received
-the bytes received will be in the form of:
-file_name + 1 byte delimiter + file_data
-This function creates a file using file_name and then writes file_data into it
-'''
-def getFile(sock, numBytes):
-    
-
-    # The buffer
-    recvBuff = ""
-
-    # The temporary buffer
-    tmpBuff = ""
-
-    # Keep receiving till all is received
-    while len(recvBuff) < numBytes:
-        
-        # Attempt to receive bytes
-        tmpBuff =  sock.recv(numBytes)
-        
-        # The other side has closed the socket
-        if not tmpBuff:
-            break
-        
-        # Add the received bytes to the buffer
-        recvBuff += tmpBuff
-    file_size = len(recvBuff)
-    i = recvBuff.find(bytes(0x1f))
-    file_name = recvBuff[0:i]
-    i += 1
-    file_data = recvBuff[i:]
-    file = open(file_name, "w")
-    file.write(file_data)
-    file.close()
-    return file_size
-
-# Create a socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to port 0
-server_socket.bind(('',0))
-
-# Retreive the ephemeral port number
-print "I chose ephemeral port: ", server_socket.getsockname()[1]
-
-# Start listening on the socket
-server_socket.listen(1)
-
-while True:
-	  
-  	# OPEN CONTROL CONNECTION WITH CLIENT #
-  	print "Server now listening for connection requests..."
-
-  	# Accept connections
-
-  	clientSock, addr = server_socket.accept()
-  	print 'Opened a control connection with client', addr
+  # The buffer
+  recvBuff = ""
   
-
-	while True:
-		file_name = ""
-		buff = clientSock.recv(100)
-		print "client> ",buff
-
-		if buff == "quit":
-			break
-		elif buff[0:4]=="get ":
-			file_name = buff[4:]
-			print "get "+file_name
-		elif buff[0:4]=="put ":
-			file_name = buff[4:]
-			print "put "+file_name
-		elif buff == "ls":
-			# Run ls command, get output, and print it
-			for line in commands.getstatusoutput(buff):
-				print "client> ",line
-				clientSock.send(str(line))
-		elif buff == "lls":
-			print "list client stuff"
-		else:
-			print "Command not recognized."
+  # The temporary buffer
+  tmpBuff = ""
+  
+  # Keep receiving till all is received
+  while len(recvBuff) < numBytes:
+    
+    # Attempt to receive bytes
+    tmpBuff =  sock.recv(numBytes)
+    
+    # The other side has closed the socket
+    if not tmpBuff:
+      break
+    
+    # Add the received bytes to the buffer
+    recvBuff += tmpBuff
+  
+  return recvBuff
 
 
-	break
-server_socket.close()
+# CHECK VALID FUNCTION ARGS #
+if len(sys.argv) < 2:
+  print "USAGE python ", sys.argv[0], " <PORT NUMBER>"
+  sys.exit()
 
-#get <file name> (downloads file <file name> from the server)
-#put <filename> (uploads file <file name> to the server)
-#ls (lists files on the server)
-#lls (lists files on the client)
-#quit (disconnects from the server and exits)
+try:
+  port = int(sys.argv[1])
+except:
+  print "Error: please enter a valid port number"
+  sys.exit()
+
+# CREATE SERVER SOCKET #
+
+server_socket = socket(AF_INET, SOCK_STREAM)
+try:
+  server_socket.bind(('', port))
+except Exception as e:
+  print "Could not bind the server socket to the specified port number"
+  print "Error: ", e
+  sys.exit()
+  
+server_socket.listen(100)
+
+while 1:
+  
+  # OPEN CONTROL CONNECTION WITH CLIENT #
+  print "Server now listening for connection requests..."
+
+  
+  control_connection, client_address = server_socket.accept()
+  print 'Opened a control connection with client', client_address
+  
+  command = ""
+
+  # LISTEN FOR A NEW COMMAND #
+  while True:
+    #Reset command buffer
+    command = ""
+
+    #Get the length of the command
+    print 'Waiting for length of command...'
+    command_len_str = ""
+    while len(command_len_str) != 3:
+      command_len_buffer = control_connection.recv(3)
+      if not command_len_buffer:
+        print 'Connection closed unexpectedly when waiting for length of command'
+        break
+      command_len_str += command_len_buffer
+
+    #Length of command has been received  
+    command_len = int(command_len_str)
+    print 'Received length of command:', command_len_str
+
+    #Get command
+    print 'Waiting for command...'
+    while len(command) != command_len:
+      command_buffer = control_connection.recv(command_len)
+      if not command_buffer:
+        print 'Connection closed unexpectedly when waiting for command'
+        break
+      command += command_buffer
+    print 'Received command', command
+
+    if command=="quit":
+      break
+    elif command.split(" ")[0]=="put":
+      # Create a socket
+      temp_socket = socket(AF_INET, SOCK_STREAM)
+      # Bind the socket to port 0
+      temp_socket.bind(('',0))
+      # Retreive the ephemeral port number
+      print "I chose ephemeral port: ", temp_socket.getsockname()[1]
+      #send port num
+      control_connection.send(str(temp_socket.getsockname()[1]))
+      # wait for the client to connect to it
+      print "Waiting for the client to connect to temp port..."
+      temp_socket.listen(1)
+
+      control_connection_t, client_address_t = temp_socket.accept()
+      print "client connected to temp port"
+
+
+
+
+
+      ## DO THE RECIEVING FILE PART HERE
+
+
+      temp_socket.close()
+
+    elif command=="ls":
+      os.system("ls")
+      #send this stuff to client
+    else:
+      #Command has been received
+      print "Command invalid."
+    
+    # OPEN DATA CONNECTION #
+    
+    # DO FUNCTION #
+
+    #CLOSE DATA CONNECTION #   
+
+
+  #Client has closed the connection
+  # CLOSE CONTROL CONNECTION #
+  control_connection.close()
+  print 'Closed connection with client', client_address
+  server_socket.close()  
+  sys.exit()
+
